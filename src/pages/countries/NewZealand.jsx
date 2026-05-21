204552825709de4d157289b2348d21ae6242c0c7
jayintopstudio <jayintopstudio>
2026-05-21 16:06:56 +0100

    Updated Newzealand Schools

--- src/pages/countries/NewZealand.jsx
+++ src/pages/countries/NewZealand.jsx
@@ -3,7 +3,25 @@ import { countryHeroImages } from '../../data/countryHeroImages'
 
 const schools = [
   'University of Canterbury College (Navitas)',
-  'Lincoln University',
+  'Lincoln University', 
+  'University of Auckland',
+'University of Otago',
+'Massey University',
+'Victoria University of Wellington – Degree Programs',
+'Victoria University of Wellington – Pathway Programs',
+'University of Waikato',
+'University of Canterbury',
+'Auckland University of Technology',
+'Unitec Institute of Technology',
+'Otago Polytechnic',
+'Manukau Institute of Technology',
+'Ara Institute of Canterbury',
+'Eastern Institute of Technology',
+'Waikato Institute of Technology (Wintec)',
+'Southern Institute of Technology',
+'Western Institute of Technology at Taranaki (WITT)',
+'The Universal College of Learning (UCOL)',
+'Toi Ohomai Institute of Technology'
 ]
 
 export default function NewZealand() {
